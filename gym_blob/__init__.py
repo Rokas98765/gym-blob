65e149e5c697a412c74006299181d584d4380e1d
Rokas98765 <33398705+Rokas98765@users.noreply.github.com>
2018-04-09 12:21:28 +0100

    Update __init__.py

--- gym_blob/__init__.py
+++ gym_blob/__init__.py
@@ -2,9 +2,6 @@ from gym.envs.registration import register
 
 register(
     id='blob-v0',
-    entry_point='gym_blob.envs:blobEnv',
+    entry_point='gym_blob.envs:BlobEnv',
 )
-register(
-    id='blob-extrahard-v0',
-    entry_point='gym_blob.envs:blobExtraHardEnv',
-)
+
